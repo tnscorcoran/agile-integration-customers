a0906c16cf128cb4308fec42b83b144b7723d213
Tom Corcoran <tncorcoran@yahoo.com>
2017-11-23 14:34:08 +0800

    Update app.js

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.use(
 
     connection(mysql,{
 
-        host: '172.30.73.100', // host mysql service
+        host: '172.30.105.86', // host mysql service
         user: 'dbuser',
         password : 'password',
         port : 3306, // port mysql service
