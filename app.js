d5311cbbc683e0b8c9d68ca15ea45514f23c7ddd
Tom Corcoran <tncorcoran@yahoo.com>
2018-09-03 19:01:59 +1000

    Update app.js

--- app.js
+++ app.js
@@ -59,7 +59,7 @@ app.use(
         user: 'root',
         password : 'Q8p1AXkQ7P5eEVVN',
         port : 3306, 
-        database: 'eval(process.env.database)'
+        database: 'process.env.database'
       
       
 
